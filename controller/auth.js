87e3a80882aef238ff9e30d9b618ad18aeaf2128
ToroERP222 <135905667+ToroERP222@users.noreply.github.com>
2023-06-08 00:13:28 -0600

    http only change

--- controller/auth.js
+++ controller/auth.js
@@ -144,11 +144,11 @@ const sendTokenResponse = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + 30 * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: false
   };
 
 
-
+  
   res
     .status(201)
     .cookie('token', token, options)
